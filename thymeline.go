e74f64b4d4da8271850672ab7b186865328533f5
n0ncetonic <n0ncetonic@users.noreply.github.com>
2018-09-24 09:57:37 -0700

    Initial Commit

---
+++ thymeline.go
@@ -0,0 +1,35 @@
+// Thymeline [[ finish this ]]
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	logGroup := router.Group("/log")
+	{
+		logGroup.POST("/terminal", termLogHandler)
+	}
+
+	eventGroup := router.Group("/event")
+	{
+		eventGroup.POST("/screenshot", scrotEventHandler)
+	}
+
+	router.Run(":8080")
+
+}
+
+func termLogHandler(c *gin.Context) {
+	// TODO: real-time terminal log handling
+	c.JSON(http.StatusOK, gin.H{"Ok": true})
+}
+
+func scrotEventHandler(c *gin.Context) {
+	// TODO: handle screenshot events
+	c.JSON(http.StatusOK, gin.H{"Ok": true})
+}
