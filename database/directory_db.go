29acdcb57cbacc1a28de62f95ba37b0223dfc992
Luis Santana <lsantana@lsantana-mbp.corp.dropbox.com>
2018-10-15 16:37:30 -0700

    Adds functionality to save OpDir to database

--- database/directory_db.go
+++ database/directory_db.go
@@ -2,5 +2,20 @@ package db
 
 // OpDir hold a map that links directory paths to operation names
 type OpDir struct {
-	Operations map[string]string `json:"operations"`
+	Operation map[string]string `json:"operation" bson:"operation"`
+}
+
+// AddToDB adds an Operation to the Directory Database
+func (od OpDir) AddToDB (s Session) error {
+	session := s.Copy()
+	defer session.Close()
+
+	collection := session.DB(dirDB).C(dirCollection)
+	
+	err := collection.Insert(od)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
