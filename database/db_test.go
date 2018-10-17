55ffc48881ca4671c7d8db190f2d5c1524cf07fb
Luis Santana <lsantana@lsantana-mbp.corp.dropbox.com>
2018-10-17 12:29:39 -0700

    Updates TestAddToDB to use test directory

--- database/db_test.go
+++ database/db_test.go
@@ -1,7 +1,9 @@
 package db
 
-import "testing"
-
+import (
+	"go/build"
+	"testing"
+)
 func TestConnect(t *testing.T) {
 	_, err := Connect()
 	if err != nil {
@@ -10,6 +12,8 @@ func TestConnect(t *testing.T) {
 }
 
 func TestAddToDB(t *testing.T) {
+	var projectPath = build.Default.GOPATH + "/src/github.com/BlacksunLabs/thymeline"
+
 	s, err := Connect()
 	defer s.Close()
 
@@ -19,9 +23,13 @@ func TestAddToDB(t *testing.T) {
 
 	opdir := &OpDir{}
 	testDir := make(map[string]string)
-	testDir["testdir"] = "/Path/to/non-existent/directory"
+	testDir["testdir"] = projectPath + "/database/testdir"
 	opdir.Operation = testDir
-	opdir.AddToDB(*s)
+	
+	err = opdir.AddToDB(*s)
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func TestRemoveOpDir(t *testing.T) {
