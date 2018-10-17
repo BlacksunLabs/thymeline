4ece3ef8a1760b1662e5002f4cee3f3d17b4ca0a
Luis Santana <lsantana@lsantana-mbp.corp.dropbox.com>
2018-10-17 13:50:53 -0700

    Adds GetOpDirs

--- database/directory_db.go
+++ database/directory_db.go
@@ -7,17 +7,17 @@ import (
 
 // OpDir hold a map that links directory paths to operation names
 type OpDir struct {
-	ID bson.ObjectId `json:"id" bson:"_id"`
+	ID        bson.ObjectId     `json:"id" bson:"_id,omitempty"`
 	Operation map[string]string `json:"operation" bson:"operation"`
 }
 
 // AddToDB adds an Operation to the Directory Database
-func (od OpDir) AddToDB (s Session) error {
+func (od OpDir) AddToDB(s Session) error {
 	session := s.Copy()
 	defer session.Close()
 
 	collection := session.DB(dirDB).C(dirCollection)
-	
+
 	err := collection.Insert(od)
 	if err != nil {
 		return err
@@ -27,7 +27,7 @@ func (od OpDir) AddToDB (s Session) error {
 }
 
 // RemoveOpDir removes a document from the Database_DB OpDir collection
-func RemoveOpDir (s Session, opname string) error {
+func RemoveOpDir(s Session, opname string) error {
 	session := s.Copy()
 	defer session.Close()
 
@@ -47,10 +47,42 @@ func RemoveOpDir (s Session, opname string) error {
 						id := doc["_id"].(bson.ObjectId)
 						collection.RemoveId(id)
 					}
-					return nil
 				}
+				return nil
 			}
 		}
 	}
 	return fmt.Errorf("no operation named %s found", opname)
+}
+
+// GetOpDirs queries the OpDir collection for a list of all operation directories
+//
+// Returns
+// A slice of strings containing paths to operation screenshot directories
+func GetOpDirs(s Session) ([]string, error) {
+	var (
+		session    = s.Copy()
+		opdirs     []string
+		data       []bson.M
+		collection = s.GetCollection(dirDB, dirCollection)
+	)
+	defer session.Close()
+
+	err := collection.Find(bson.M{}).All(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, doc := range data {
+		for key, value := range doc {
+			if key == "operation" {
+				for _, value := range value.(bson.M) {
+					fmt.Printf("%v", value.(string))
+					opdirs = append(opdirs, value.(string))
+				}
+			}
+		}
+	}
+
+	return opdirs, nil
 }
