3dce0a5a86141adb2eb4e66d0e5487628cf63a3c
Luis Santana <lsantana@lsantana-mbp.corp.dropbox.com>
2018-10-22 15:31:24 -0700

    Gets monitor to completely working state with processing

--- screenshot/monitor.go
+++ screenshot/monitor.go
@@ -1,23 +1,28 @@
 package screenshot
 
 import (
-	"time"
 	"fmt"
+	"time"
+
+	"github.com/BlacksunLabs/thymeline/database"
 	"github.com/radovskyb/watcher"
 )
 
 var w = watcher.New()
 
 // WatchDirs watches directories for new screenshots
-func WatchDirs() {
+func WatchDirs() error {
 	w.SetMaxEvents(1)
 	w.FilterOps(watcher.Create)
 
 	go func() {
 		for {
 			select {
-			case event := <- w.Event:
-				fmt.Println(event)
+			case event := <-w.Event:
+				ok, err := Process(event.Path)
+				if !ok || err != nil {
+					fmt.Printf("%v", err)
+				}
 			case err := <-w.Error:
 				fmt.Println(err)
 			case <-w.Closed:
@@ -26,14 +31,27 @@ func WatchDirs() {
 		}
 	}()
 
-	err := w.Start(time.Millisecond * 100)
+	opdirs, err := getOpDirs()
+	if err != nil {
+		return err
+	}
+
+	for _, dir := range opdirs {
+		err = AddDir(w, dir)
+		if err != nil {
+			return fmt.Errorf("Unable to add %s : %v", dir, err)
+		}
+	}
+
+	err = w.Start(time.Millisecond * 100)
 	if err != nil {
 		fmt.Println(err)
 	}
+	return nil
 }
 
 // AddDir adds a directory to be watched via its path
-func AddDir(path string) error {
+func AddDir(w *watcher.Watcher, path string) error {
 	err := w.Add(path)
 	if err != nil {
 		return err
@@ -41,12 +59,23 @@ func AddDir(path string) error {
 	return nil
 }
 
+// GetOpDirs gets all current OpDirs from Directory_DB
+func getOpDirs() ([]string, error) {
+	s, err := db.Connect()
+	if err != nil {
+		return nil, err
+	}
+	defer s.Close()
+
+	opdirs, err := db.GetOpDirs(*s)
+	if err != nil {
+		return nil, err
+	}
+
+	return opdirs, nil
+}
+
 // IsOpDir determines whether a directory is part of an active operation
 func IsOpDir(path string) (bool, error) {
 	return true, nil
-}
-
-// GetOpNameFromDir gets an operation name for a given directory
-func GetOpNameFromDir(path string) (string, error) {
-	return "", nil
 }
