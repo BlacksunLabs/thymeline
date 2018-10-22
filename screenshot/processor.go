ab62695134184909881d82c6092413ceaec8d4d2
Luis Santana <lsantana@lsantana-mbp.corp.dropbox.com>
2018-10-22 13:38:29 -0700

    Process now saves Screenshots to Event_Cache

--- screenshot/processor.go
+++ screenshot/processor.go
@@ -2,24 +2,54 @@ package screenshot
 
 import (
 	"fmt"
+	"path/filepath"
+	"time"
+
+	"github.com/BlacksunLabs/thymeline/database"
 )
 
 // Process processes a screenshot event and stores the event in the local database
-func Process(path string) error {
-	sc := &Screenshot{}
-	description, ok, err := sc.GetDescription()
+func Process(path string) (ok bool, err error) {
+	s, err := db.Connect()
 	if err != nil {
-		return err
-	} else if !ok {
-		return fmt.Errorf("user clicked cancel button")
+		fmt.Printf("%v", err)
+	}
+	defer s.Close()
+
+	sc := &Screenshot{}
+
+	for i := 0; i < 3; i++ {
+		ok, err = sc.GetDescription()
+		if err != nil {
+			return false, err
+		} else if !ok {
+			if i == 2 {
+				return false, fmt.Errorf("user cancelled 3 times")
+			}
+			continue
+		} else {
+			break
+		}
 	}
 
-	sc.Rename(path, description)
+	sc.Rename(path)
 
 	err = sc.HashFile()
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	return nil
+	opname, err := db.GetOpNameFromPath(*s, filepath.Dir(path))
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	sc.OpName = opname
+
+	sc.Timestamp = time.Now().Local()
+	err = db.SaveScreenshot(s, sc)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
