c8557765ba731baf8af8128cd48cd8d65c06f37c
Luis Santana <lsantana@lsantana-mbp.corp.dropbox.com>
2018-10-22 15:30:24 -0700

    Honestly not sure

--- database/db.go
+++ database/db.go
@@ -5,21 +5,21 @@ import (
 	"github.com/globalsign/mgo"
 )
 
-// Session wraps an pointer to an mgo.Session and adds convenience methods 
+// Session wraps an pointer to an mgo.Session and adds convenience methods
 type Session struct {
 	*mgo.Session
 }
 
 const (
-	host = "localhost:27017"
-	dirDB = "Directory_DB"
-	dirCollection = "OpDirs"
-	eventDB = "Event_Cache"
+	host            = "localhost:27017"
+	dirDB           = "Directory_DB"
+	dirCollection   = "OpDirs"
+	eventDB         = "Event_Cache"
 	scrotCollection = "Screenshots"
 )
 
 // newSession returns a pointer to a new Session object given a pointer to an mgo.Session
-func newSession (s *mgo.Session) *Session {
+func newSession(s *mgo.Session) *Session {
 	return &Session{s}
 }
 
@@ -38,7 +38,6 @@ func Connect() (*Session, error) {
 }
 
 // GetCollection gets a collection from a database given a valid session and returns a pointer to the collection
-func (s *Session) GetCollection(db string, collection string) (*mgo.Collection) {
+func (s *Session) GetCollection(db string, collection string) *mgo.Collection {
 	return s.DB(db).C(collection)
 }
-
