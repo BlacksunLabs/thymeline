06c0261988d5b2f1d2d038d1108ce76885835326
Luis Santana <lsantana@lsantana-mbp.corp.dropbox.com>
2018-10-25 15:56:44 -0700

    Adds `--new` flag for adding an opdir to the database

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "github.com/BlacksunLabs/thymeline/screenshot"
+import (
+	"fmt"
+
+	"github.com/BlacksunLabs/thymeline/database"
+	"github.com/BlacksunLabs/thymeline/screenshot"
+	"github.com/BlacksunLabs/thymeline/ui"
+	flag "github.com/spf13/pflag"
+)
+
+// Commandline flags
+var (
+	newopFlag bool
+)
+
+// Parse commandline options
+func init() {
+	flag.BoolVarP(&newopFlag, "new", "n", false, "used to create and monitor a new operation directory")
+	flag.Parse()
+}
+
+func createop() {
+	dir, err := ui.ChooseDirectory()
+	if err != nil {
+		fmt.Printf("Unable to create directory `%s` : %v", dir, err)
+		return
+	}
+
+	opName, ok, err := ui.GetText("Name of operation")
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	} else if !ok {
+		fmt.Println("user clicked cancel.. Skipping adding operation directory to monitor")
+	} else { // No error, user didn't cancel. Try adding the op
+		s, err := db.Connect()
+		if err != nil {
+			fmt.Printf("unable to connect to database: %v", err)
+			return
+		}
+		defer s.Close()
+
+		opdir := db.OpDir{
+			Operation: db.Op{
+				Name: opName,
+				Path: dir,
+			},
+		}
+		err = opdir.AddToDB(*s)
+		if err != nil {
+			fmt.Printf("unable to add operation to database: %v", err)
+		}
+		return
+	}
+}
 
 func main() {
+	// Handle commandline options
+	if newopFlag {
+		createop()
+	}
+
 	screenshot.WatchDirs()
 }
